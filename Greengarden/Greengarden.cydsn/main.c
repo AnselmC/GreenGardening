a01036f78655bc6f6a37411b0f49263ed7b1fceb
Christoph Clement <christoph.clement@tum.de>
2018-01-03 17:35:47 +0100

    Added Greengarden Folder

---
+++ Greengarden/Greengarden.cydsn/main.c
@@ -0,0 +1,35 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+#include <stdio.h>
+
+int main(void)
+{
+    uint16 sensorval;
+    char msg[10];
+    
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    UART_Start();
+    
+    for(;;)
+    {
+        /* Place your application code here. */
+        sensorval = Pin_Moisture_Sensor_Read();
+        sprintf(msg, "%3d\r\n", sensorval);
+        UART_UartPutString(msg);
+        CyDelay(500);
+    }
+}
+
+/* [] END OF FILE */
