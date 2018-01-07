f194e16303e19ed11b368585d709eaa98aff370c
Christoph Clement <christoph.clement@tum.de>
2018-01-07 17:30:25 +0100

    CheckLight() and CheckMoisture() added

--- Greengarden/Greengarden.cydsn/main.c
+++ Greengarden/Greengarden.cydsn/main.c
@@ -1,34 +1,42 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
 #include "project.h"
-#include <stdio.h>
+
+void checkLight()
+{
+    if (Pin_Light_Sensor_Read())
+    {
+        Pin_LED_blue_Write(0);   
+    } 
+    else 
+    {
+        Pin_LED_blue_Write(1);
+    }
+}
+void checkMoisture()
+{
+    if (Pin_Moisture_Sensor_Read())
+    {
+        Pin_pump_Write(1);   
+    } 
+    else 
+    {
+        Pin_pump_Write(0);
+    }
+}
 
 int main(void)
 {
-    uint16 sensorval;
-    char msg[10];
-    
-    CyGlobalIntEnable; /* Enable global interrupts. */
+    /* Enable global interrupts. */   
+    CyGlobalIntEnable; 
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-    UART_Start();
     
     for(;;)
     {
         /* Place your application code here. */
-        sensorval = Pin_Moisture_Sensor_Read();
-        sprintf(msg, "%3d\r\n", sensorval);
-        UART_UartPutString(msg);
-        CyDelay(500);
+        checkLight();
+        checkMoisture();
+        
+        CyDelay(1000);
     }
 }
 
